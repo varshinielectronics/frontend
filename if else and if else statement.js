1f3c030552df09a425f9ebd3068b1ae206f14b42
varshinielectronics <varshinivarshini87458@gmail.com>
2025-10-16 12:16:04 +0530

    Add files via upload

---
+++ if else and if else statement.js
@@ -0,0 +1,14 @@
+let marks=85;
+if(marks>=90){
+    console.log("you got A+");
+}else if(marks>=80){
+    console.log("you got A");
+}else if(marks>=70){
+    console.log("you got B+");
+}else if(marks>=60){
+    console.log("you got B");
+}else if(marks>=50){
+    console.log("you got C");
+}else{
+    console.log("you are failed");
+}
---
+++ simple program.js
@@ -0,0 +1,8 @@
+let varshini="23";
+console.log(varshini);
+let latha="24";
+console.log(latha);
+console.log(varshini+latha);
+console.log("your lucky number is ",varshini+latha);
+console.log("your lucky number is ",varshini);
+console.log("your lucky number is ",latha);
